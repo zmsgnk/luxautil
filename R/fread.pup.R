730c967cfd3f4ddb5c38d2db961ef8579a990598
koji.makiyama <koji.makiyama@luxa.co.jp>
2014-07-28 12:42:28 +0900

    change convert string

--- R/fread.pup.R
+++ R/fread.pup.R
@@ -29,6 +29,6 @@ fread.pup <- function(input = "test.csv", sep = "auto", sep2 = "auto", nrows = -
                 verbose = verbose, autostart = autostart, skip = skip, select = select, 
                 drop = drop, colClasses = colClasses, integer64 = integer64, showProgress = showProgress)
   first <- colnames(data)[1]
-  setnames(data, first, first %>% str_replace("\xefｻｿ" , "") %>% str_replace_all("\"", ""))
+  setnames(data, first, first %>% str_replace("攼㹦ｻｿ" , "") %>% str_replace_all("\"", ""))
   data
 }
